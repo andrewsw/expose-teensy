76afc6004ed1cc03a40b6e576e039c9c2fa13867
Andrew Sackville-West <andrew@swclan.homelinux.org>
2010-11-11 19:31:06 -0800

    remove spurious newline

--- usb_driver/teensy.c
+++ usb_driver/teensy.c
@@ -39,7 +39,7 @@ static void teensy_interrupt_in_callback (struct urb *urb)
 		
 		for (i=0; i < urb->actual_length - 3;i+=4) {
 			
-			sprintf(data, "%2x %2x %2x %2x\n", dev->in_buf[i], dev->in_buf[i+1], dev->in_buf[i+2],dev->in_buf[i+3]);
+			sprintf(data, "%2x %2x %2x %2x", dev->in_buf[i], dev->in_buf[i+1], dev->in_buf[i+2],dev->in_buf[i+3]);
 			data[64]=0x00;
 			DPRINT("data: %s\n", data);
 		}
