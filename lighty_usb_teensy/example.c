fad6dba58e551ef376828dbe9408e23bb0b95c97
jim <jim@duhclem.hsd1.or.comcast.net>
2010-11-17 21:44:59 -0800

    changed port names to Atmel names.

--- lighty_usb_teensy/example.c
+++ lighty_usb_teensy/example.c
@@ -23,6 +23,8 @@
 
 // Modified to provide PWM Motor control using Timer 1
 //	11/9/2010     -jkl
+// Fixed port definitions to comply with Atmel desires. 11/16/2010 -jkl
+//	ADCs only read when asked.
 
 #include <avr/io.h>
 #include <avr/pgmspace.h>
@@ -32,29 +34,7 @@
 #include "analog.h"
 
 #define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
-#if 0
-// this is done just to get this to build
-#define PD1	1
-#define PD2	2
-#define PD3	3
-#define PD4	4
-#define PD6	6
-#define PD7	7
-#define PC6	6
-#define PC7	7
-#define PB5	5
-#define PB6	6
-#endif
-#define PD1	PORTD1
-#define PD2	PORTD2
-#define PD3	PORTD3
-#define PD4	PORTD4
-#define PD6	PORTD6
-#define PD7	PORTD7
-#define PC6	PORTC6
-#define PC7	PORTC7
-#define PB5	PORTB5
-#define PB6	PORTB6
+
 volatile uint8_t do_output=0;
 uint8_t buffer[64];
 
@@ -79,7 +59,7 @@ int main(void)
 // Set up Timer 1 for PWM control.
 // P&F Correct, Runs 5KHz. Divide 16Mhz clock by 8, cycle = 200.
 // Make OCR1A & B outputs.
-	DDRB |= ((1<<PB5) | (1<<PB6));
+	DDRB |= ((1<<PORTB5) | (1<<PORTB6));
 	//TCCR1A = (1<<COM1A1) | (1<<COM1A0) | (1<<COM1B1) | (1<<COM1B0);
 	TCCR1A = (1<<COM1A1)  | (1<<COM1B1) ; 
 	TCCR1B = (1<<WGM13) | (1<<CS11);
@@ -88,10 +68,10 @@ int main(void)
 	OCR1B = 0; 
 
 // Set up Control Signals. PD6&7 for OCR1B; PC6&7 for OCR1A.
-	DDRD |= (1<<PD6) | (1<<PD7);
-	DDRC |= (1<<PC6) | (1<<PC7);
-	PORTD &= ~((1<<PD6) | (1<<PD7));	// take low to start (off)
-	PORTC &= ~((1<<PC6) | (1<<PC7));
+	DDRD |= (1<<PORTD6) | (1<<PORTD7);
+	DDRC |= (1<<PORTC6) | (1<<PORTC7);
+	PORTD &= ~((1<<PORTD6) | (1<<PORTD7));	// take low to start (off)
+	PORTC &= ~((1<<PORTC6) | (1<<PORTC7));
 
         // Configure timer 0 to generate a timer overflow interrupt every
         // 256*1024 clock cycles, or approx 61 Hz when using 16 MHz clock
@@ -108,55 +88,56 @@ int main(void)
 // PD7 --> Blue 1
 			switch(buffer[0]){
 				case 'a':		// Motors Fwd, 87.5%
-					PORTD &= ~((1<<PD6) | (1<<PD7));	// turns off motors
-					PORTC &= ~((1<<PC6) | (1<<PC7));
+					PORTD &= ~((1<<PORTD6) | (1<<PORTD7));	// turns off motors
+					PORTC &= ~((1<<PORTC6) | (1<<PORTC7));
 					OCR1A = 175;
 					OCR1B = 175; 
 					_delay_ms(500);
-					PORTD |= (1<<PD6);	
-					PORTC |= (1<<PC6);
+					PORTD |= (1<<PORTD6);	
+					PORTC |= (1<<PORTC6);
 					break;
 				case 'b':		// Motors Fwd, 50%
-					PORTD &= ~((1<<PD6) | (1<<PD7));	// turns off motors
-					PORTC &= ~((1<<PC6) | (1<<PC7));
+					PORTD &= ~((1<<PORTD6) | (1<<PORTD7));	// turns off motors
+					PORTC &= ~((1<<PORTC6) | (1<<PORTC7));
 					OCR1A = 100;
 					OCR1B = 100; 
 					_delay_ms(500);
-					PORTD |= (1<<PD6);	
-					PORTC |= (1<<PC6);
+					PORTD |= (1<<PORTD6);	
+					PORTC |= (1<<PORTC6);
 					break;
 				case 'c':		// Motors Rev, 50%
-					PORTD &= ~((1<<PD6) | (1<<PD7));	// turns off motors
-					PORTC &= ~((1<<PC6) | (1<<PC7));
+					PORTD &= ~((1<<PORTD6) | (1<<PORTD7));	// turns off motors
+					PORTC &= ~((1<<PORTC6) | (1<<PORTC7));
 					OCR1A = 100;
 					OCR1B = 100; 
 					_delay_ms(500);
-					PORTD |= (1<<PD7);	
-					PORTC |= (1<<PC7);
+					PORTD |= (1<<PORTD7);	
+					PORTC |= (1<<PORTC7);
 					break;
 				case 'd':
-					DDRD |= (1<<PD1);
-					PORTD |= (1<<PD1);
-					PORTD &= ~((1<<PD6) | (1<<PD7));	// turns off motors
-					PORTC &= ~((1<<PC6) | (1<<PC7));
+					DDRD |= (1<<PORTD1);
+					PORTD |= (1<<PORTD1);
+					PORTD &= ~((1<<PORTD6) | (1<<PORTD7));	// turns off motors
+					PORTC &= ~((1<<PORTC6) | (1<<PORTC7));
 					OCR1A = 0;
 					OCR1B = 0; 
 					_delay_ms(500);
-					PORTD &= ~(1<<PD1);
+					PORTD &= ~(1<<PORTD1);
 					_delay_ms(500);
 					break;
 				case 'e':
-					DDRD |= (1<<PD2);
-					PORTD |= (1<<PD2);
+					DDRD |= (1<<PORTD2);
+					PORTD |= (1<<PORTD2);
 					_delay_ms(500);
-					PORTD &= ~(1<<PD2);
+					PORTD &= ~(1<<PORTD2);
 					_delay_ms(500);
+					do_output = 1;		// tell the ADCs to sample
 					break;
 				case 'f':
-					DDRD |= (1<<PD3);
-					PORTD |= (1<<PD3);
+					DDRD |= (1<<PORTD3);
+					PORTD |= (1<<PORTD3);
 					_delay_ms(500);
-					PORTD &= ~(1<<PD3);
+					PORTD &= ~(1<<PORTD3);
 					_delay_ms(500);
 					break;
 				default:
@@ -169,15 +150,15 @@ int main(void)
 			// send a packet, first 2 bytes 0xABCD
 			buffer[0] = 0xAB;
 			buffer[1] = 0xCD;
-			buffer[2] = r;
- 			// put A/D measurements into next 24 bytes
-			for (i=0; i<12; i++) {
+			//buffer[2] = r; // This seems pointless - overwritten?
+ 			// put A/D measurements into next 12 bytes
+			for (i=0; i<6; i++) {
 				val = analogRead(i);
 				buffer[i * 2 + 2] = val >> 8;
 				buffer[i * 2 + 3] = val & 255;
 			}
 			// most of the packet filled with zero
-			for (i=26; i<62; i++) {
+			for (i=14; i<62; i++) {
 				buffer[i] = 0;
 			}
 			//// put a count in the last 2 bytes
@@ -191,6 +172,7 @@ int main(void)
 }
 
 // This interrupt routine is run approx 61 times per second.
+/*    Turn off for now
 ISR(TIMER0_OVF_vect)
 {
 	static uint8_t count=0;
@@ -200,7 +182,7 @@ ISR(TIMER0_OVF_vect)
 		count = 0;
 		do_output = 1;
 	}
-}
+} */
 
 
 
