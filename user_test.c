19076563ddd49f0771a213cdb8f3387ad1b3d93b
Andrew Sackville-West <andrew@swclan.homelinux.org>
2010-11-27 10:51:41 -0800

    add licenses to Harry and Remi's code

--- user_test.c
+++ user_test.c
@@ -1,7 +1,29 @@
-/* userland test file that read from an ADC 
- * to set the speed of the motor.
+/*
+ *  user_test.c  
+ *  userland test file that read from an ADC 
+ *  to set the speed of the motor.
+ * 
+ *  This is a linux module to expose portions of the linux hardware to
+ *  userspace as character devices.
  *
- * Author: Rémi Auduon <remi.auduon@pdx.edu> 
+ *  Copyright (C) 2010  Rémi Auduon <remi.auduon@pdx.edu> 
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ *  02110-1301 USA.
+ *
+ * 
  */
 #include <string.h>
 #include <sys/types.h>
